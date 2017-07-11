fc7cf546bd791baa219fd44d83d3462c1f2ae0fb
Juntunen Otto (Samlink) <otto.juntunen@samlink.fi>
2017-07-11 10:15:26 +0300

    Initial commit

---
+++ AbstractActivity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Activities;
+
+namespace AbstractActivityForUiPath
+{
+    public abstract class AbstractActivity : CodeActivity
+    {
+        public string MyStringProperty { get; set; }
+        protected override void Execute(CodeActivityContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
